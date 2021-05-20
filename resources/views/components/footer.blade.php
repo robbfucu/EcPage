497466c75ac7ca01b00380829dc05bd162197798
Roberto Fucugauchi <robbfucu@gmail.com>
2021-05-20 15:28:47 -0500

    se regreso formulario y se cambiaron los placeholder

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,24 +1,67 @@
 <footer class="page_footer cs mx-auto section_padding_top_30 columns_margin_0">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 col-md-6">
+            <div class="col-lg-3 col-md-4">
                 <a href="./" class="logo">
-                    <img src="images/logo.png" alt="">
-                    <span class="logo_text">
-                        <span class="highlight kanit bold">Oildrop
-                            <br> global</span>
-                        <span class="small-text">test with the best</span>
-                    </span>
+                    <img src="./images/ici_logo.png" alt="">
                 </a>
                 <p class="topmargin_30">
-                    Everyday is a new day for us and we work really hard to satisfy our customer everywhere.
+                Estamos comprometidos con la calidad en nuestros servicios con la
+                convicción de dejar un trabajo que exceda las expectativas de nuestros clientes.
                 </p>
                 <div class="page_social_icons greylinks">
                     <a class="social-icon border-icon rounded-icon soc-facebook" href="#" title="Facebook"></a>
                 </div>
             </div>
 
-            <div class="col-lg-offset-0 col-lg-6 col-md-offset-6 col-md-6">
+            <div class="col-lg-6 col-md-8">
+							<div class="ls with_padding footer_form">
+								<h2 class="section_header bottommargin_30 text-center">Contacto</h2>
+								<form class="contact-form row columns_margin_0 columns_padding_20" method="post" action="/">
+									<div class="col-sm-6">
+										<div class="form-group">
+											<label for="name">Nombre Completo
+												<span class="required">*</span>
+											</label>
+											<input type="text" aria-required="true" size="30" value="" name="name" id="name" class="form-control" placeholder="Nombre Completo">
+										</div>
+										<div class="form-group">
+											<label for="email">E-mail
+												<span class="required">*</span>
+											</label>
+											<input type="email" aria-required="true" size="30" value="" name="email" id="email" class="form-control" placeholder="E-mail">
+										</div>
+										<div class="form-group">
+											<label for="phone">Numero de Teléfono
+												<span class="required">*</span>
+											</label>
+											<input type="text" size="30" value="" name="phone" id="phone" class="form-control" placeholder="Numero de Teléfono">
+										</div>
+										<div class="form-group">
+											<label for="company-name">Nombre de su Compañía
+												<span class="required">*</span>
+											</label>
+											<input type="text" size="30" value="" name="company-name" id="company-name" class="form-control" placeholder="Nombre de su Compañía">
+										</div>
+										<div class="form-group">
+											<label for="title">Nombre del Proyecto
+												<span class="required">*</span>
+											</label>
+											<input type="text" size="30" value="" name="title" id="title" class="form-control" placeholder="Nombre del Proyecto">
+										</div>
+									</div>
+									<div class="col-sm-6">
+										<div class="form-group">
+											<label for="message">Especificaciones</label>
+											<textarea aria-required="true" rows="15" cols="45" name="message" id="message" class="form-control" placeholder="Especificaciones"></textarea>
+										</div>
+										<button type="submit" id="contact_form_submit" name="contact_submit" class="theme_button color1 margin_0">Enviar</button>
+									</div>
+								</form>
+							</div>
+						</div>
+
+            <div class="col-lg-offset-0 col-lg-3 col-md-offset-2 col-md-8">
                 <div class="row topmargin_40">
                     <div class="col-lg-12 col-md-6">
                         <div class="media thin_teaser inline-block bottommargin_20">

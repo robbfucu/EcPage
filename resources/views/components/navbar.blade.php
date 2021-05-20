c04ebb6a47a24c12a541271ae1fcab594bd2e73f
Roberto Fucugauchi <robbfucu@gmail.com>
2021-05-20 15:29:17 -0500

    se agrego barra superior, se agrego info y se elimino logo de la barra de navegacion

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,52 @@
+<section class="page_toplogo cs table_section table_section_md section_padding_top_30 section_padding_bottom_30">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 col-lg-3 text-center text-md-left">
+							<a href="./" class="logo">
+								<img src="./images/ici_logo.png" alt="">
+							</a>
+						</div>
+						<div class="col-md-2 col-lg-3 text-center text-md-left hidden-md">
+							<div class="media thin_teaser inline-block">
+								<div class="media-left media-middle">
+									<div class="teaser_icon border_icon round">
+										<i class="icon-map-marker highlight"></i>
+									</div>
+								</div>
+								<div class="media-body media-middle">
+									<p class="kanit text-uppercase bold fontsize_14">
+                                        Senda de la lluvia
+										<span class="highlight fontsize_18">5/2</span>
+										<br> Villa Parrilla
+										<span class="highlight">Tabasco
+											<span class="fontsize_18">86284</span>
+										</span>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-lg-3 text-center text-md-left">
+							<div class="media thin_teaser inline-block">
+								<div class="media-left media-middle">
+									<div class="teaser_icon border_icon round">
+										<i class="icon-phone highlight"></i>
+									</div>
+								</div>
+								<div class="media-body media-middle">
+									<p class="kanit text-uppercase bold fontsize_14">
+										Comunicate con nosotros
+										<br>
+										<span class="fontsize_18 highlight">+52 (993) 338 20 49</span>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-lg-3 text-center text-md-left">
+                            <a href="tel:9933382049" class="fontsize_18 highlight2 theme_button color1">Llame Ahora</a>
+						</div>
+					</div>
+				</div>
+			</section>
 <header class="page_header cs main_color2 bordered_items columns_padding_0 toggle_menu_left not_hide_menuitems with_top_border">
     <div class="container">
         <div class="row">
@@ -40,11 +89,6 @@
                                 <a href="{{route('contacto')}}">Contáctanos</a>
                             </li>
                             <!-- eof contacts -->
-                            <li>
-                                <span>
-                                    LOGO
-                                </span>
-                            </li>
                         </ul>
                     </nav>
                     <!-- eof main nav -->

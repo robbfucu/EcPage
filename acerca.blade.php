41bcfca7b0c0231f96105c5234aa29632cbf94de
robbfucu <robbfucu@gmail.com>
2021-07-31 01:05:09 -0500

    cambios en views
    ajustado tamanio de fuente en welcome, palabras en ingles en servicios y alineacion en acerca

---
+++ acerca.blade.php
@@ -0,0 +1,151 @@
+@extends('layouts.app')
+@section('content')
+<section class="page_breadcrumbs ls ms ms_dark section_padding_25">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1 class="highlight2 bold bottommargin_5">ICI INDUSTRIAL</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ls section_padding_top_50 section_padding_bottom_145">
+    <div class="container">
+        <div class="row columns_padding_25">
+            <div class="col-md-3">
+                <img src="./images/inge.png" alt="">
+            </div>
+            <div class="col-md-6">
+                <p class="entry-excerpt highlight2 topmargin_40 bottommargin_30" style="text-align: justify">
+                    <a href="">Es una compañía dedicada a la automatización de procesos
+                        industriales, con amplia experiencia en instrumentación de campo.
+                        Integramos a los mejores fabricantes para suministro de refaccionamiento
+                        para sus procesos y equipos de control.</a>
+                </p>
+                <p>
+                    Estamos comprometidos con la calidad en nuestros servicios con la convicción
+                    de dejar un trabajo que exceda las expectativas de nuestros clientes.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h4 class="highlight2">
+                    Diseñando sistemas autónomos eficientes para los clientes
+                </h4>
+            </div>
+        </div>
+        <div class="row topmargin_10 bottommargin_30">
+            <p class="entry-excerpt highlight2 topmargin_5" style="text-align: justify">
+                Fundada en febrero del 2008, con la oportunidad de integrar
+                equipos industriales de alta calidad, con marcas reconocidas
+                en el mercado, diseñando sistemas autónomos eficientes para
+                los clientes. En la actualidad ICI Industrial ha desarrollado
+                proyectos llave en mano, integrando las mejores tecnologías en el
+                mercado de petróleo y gas. Además de contar con un área de ventas
+                especialistas en las marcas que representamos, ofreciendo al cliente,
+                el mejor producto para controlar, monitorear y medir su proceso.
+            </p>
+        </div>
+        <hr>
+        <div class="row columns_padding_0 topmargin_60">
+            <div class="col-md-6 col-lg-5">
+                <p class="entry-excerpt highlight2 topmargin_5" style="text-align: justify">
+                    Siempre estamos interesados ​​en nuevos proyectos, grandes o pequeños.
+                    No dude en ponerse en contacto con nosotros en relación con su proyecto.
+                </p>
+            </div>
+            <div class="col-md-6 col-lg-5 col-lg-offset-1">
+                <p>
+                    Si desea contactarnos por favor llame al siguiente número:
+                    <span class="kanit fontsize_18 highlight bold">+52 (993) 338 20 49</span> o
+                    <a href="#" class="bold highlight underlined">envíenos un correo</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ls ms section_padding_145">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="with_padding text-center cs main_color2 with_bottom_border">
+                    <span class="image_icon">
+                        <img src="./images/icon1.png" alt="">
+                    </span>
+                    <h3 class="highlight medium">Nuestra Misión</h3>
+                    <div class="border-paragraphs middle-borders bold bottommargin_0 greylinks">
+                        <p>
+                            <a href="#">Automatizar todos los procesos.</a>
+                        </p>
+                        <p>
+                            <a href="#">Vender instrumentos de la mejor calidad en el mercado.</a>
+                        </p>
+                        <p>
+                            <a href="#">Excelencia en el servicio de ingeniería y ventas.</a>
+                        </p>
+                        <p>
+                            <a href="#">Diseñar y fabricar productos o proyectos de calidad
+                                <br>para el mercado nacional e internacional.</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="with_padding text-center cs main_color2 with_bottom_border">
+                    <span class="image_icon">
+                        <img src="./images/icon2.png" alt="">
+                    </span>
+                    <h3 class="highlight medium">Nuestra Visión</h3>
+                    <div class="border-paragraphs middle-borders bold bottommargin_0 greylinks">
+                        <p>
+                            <a href="#">ICI Industrial será una empresa innovadora
+                                en tecnología, flexible al cambio, líderes en automatización.
+                            </a>
+                        </p>
+                        <p>
+                            <a href="#">Empresa comprometida con el servicio al cliente.</a>
+                        </p>
+                        <p>
+                            <a href="#">Expandir nuestros servicios y productos a diferentes sectores
+                                industriales.</a>
+                        </p>
+                        <p>
+                            <a href="#">Empresa confiable para distribuir instrumentos en el sector
+                                industrial.</a>
+                        </p>
+                        <p>
+                            <a href="#">Crecer nuestra fabricación de sistemas de control con el más alto
+                                estándar de calidad en nuestros productos fabricados</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="with_padding text-center cs main_color2 with_bottom_border">
+                    <span class="image_icon">
+                        <img src="./images/icon3.png" alt="">
+                    </span>
+                    <h3 class="highlight medium">Objetivos</h3>
+                    <div class="border-paragraphs middle-borders bold bottommargin_0 greylinks">
+                        <p>
+                            <a href="#">Convertir nuestra empresa en una rentable para inversionistas.</a>
+                        </p>
+                        <p>
+                            <a href="#">Desarrollar a nuestro personal como primer paso al éxito.</a>
+                        </p>
+                        <p>
+                            <a href="#">Incrementar nuestra facturación al 20% anual./a>
+                        </p>
+                        <p>
+                            <a href="#">Desarrollar productos o proyectos sustentables y duraderos.</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ welcome.blade.php
@@ -0,0 +1,380 @@
+@extends('layouts.app')
+@section('content')
+<section class="intro_section page_mainslider cs">
+    <div class="flexslider">
+        <img src="images/Hand.png" alt="" class="slide-person visible-lg-block to_animate" data-animation="fadeInRight">
+        <ul class="slides">
+            <li>
+                <img src="images/intro_bg.png" alt="" class="slide-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="slide_description_wrapper">
+                                <div class="slide_description">
+                                    <div class="intro-layer">
+                                        <h3 class="bold big" style="font-size: 100px">
+                                            DISEÑAMOS
+                                        </h3>
+                                        <p class="bold" style="font-size: 28px">
+                                            Tableros de Control para diferentes procesos,
+                                            con capacidad de expansión a nuevas tecnologías
+                                            en navegación web.
+                                        </p>
+                                    </div>
+                                </div>
+                                <!-- eof .slide_description -->
+                            </div>
+                        </div>
+                        <!-- eof .col-* -->
+                    </div>
+                    <!-- eof .row -->
+                </div>
+                <!-- eof .container -->
+            </li>
+            <li>
+                <img src="images/intro_bg2.png" alt="" class="slide-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="slide_description_wrapper">
+                                <div class="slide_description">
+                                    <div class="intro-layer">
+                                        <h3 class="bold big" style="font-size: 75px">
+                                            ACTUALIZAMOS
+                                        </h3>
+                                        <p class="bold" style="font-size: 28px">
+                                        Motores de gas para mejorar su rendimiento.
+                                        </p>
+                                    </div>
+                                </div>
+                                <!-- eof .slide_description -->
+                            </div>
+                        </div>
+                        <!-- eof .col-* -->
+                    </div>
+                    <!-- eof .row -->
+                </div>
+                <!-- eof .container -->
+            </li>
+        </ul>
+    </div>
+    <!-- eof flexslider -->
+</section>
+
+<section class="ls section_padding_top_145 section_padding_bottom_90 columns_margin_bottom_60">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h2 class="section_header">
+                    Nosotros
+                </h2>
+                <hr class="divider_2_30 left-important main_bg_color divider_35">
+                <a href="{{route('acerca')}}" class="theme_button color1 topmargin_10">Mas Informacion</a>
+            </div>
+            <div class="col-md-6">
+                <p class="entry-excerpt highlight2 bottommargin_30">
+                    <a href="">
+                        Excelencia en el servicio de ingeniería y ventas.
+                    </a>
+                </p>
+                <p>
+                    ICI Industrial es una compañía dedicada a la automatización de procesos industriales,
+                    con amplia experiencia en instrumentación de campo. Somos una empresa comprometida con
+                    la calidad en nuestros servicios con la convicción de dejar un trabajo que exceda las
+                    expectativas de nuestros clientes.
+                </p>
+            </div>
+            <div class="col-md-3">
+                <img src="./images/inge.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ls ms section_padding_145">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-offset-2 col-sm-8 col-md-offset-0 col-md-3 text-center text-sm-left">
+                <h2 class="section_header">
+                    Nuestra Identidad
+                </h2>
+                <hr class="divider_2_30 left main_bg_color divider_35">
+            </div>
+            <div class="col-sm-offset-2 col-sm-8 col-md-offset-0 col-md-3">
+                <div class="with_padding text-center cs main_color2 with_bottom_border">
+                    <span class="image_icon">
+                        <img src="./images/icon1.png" alt="">
+                    </span>
+                    <h3 class="highlight medium">Nuestra Misión</h3>
+                    <div class="border-paragraphs middle-borders bold bottommargin_0 greylinks">
+                        <p>
+                            <a href="#">Automatizar todos los procesos.</a>
+                        </p>
+                        <p>
+                            <a href="#">Vender instrumentos de la mejor calidad en el mercado.</a>
+                        </p>
+                        <p>
+                            <a href="#">Excelencia en el servicio de ingeniería y ventas.</a>
+                        </p>
+                        <p>
+                            <a href="#">Diseñar y fabricar productos o proyectos de calidad
+                                <br>para el mercado nacional e internacional.</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-offset-2 col-sm-8 col-md-offset-0 col-md-3">
+                <div class="with_padding text-center cs main_color2 with_bottom_border">
+                    <span class="image_icon">
+                        <img src="./images/icon2.png" alt="">
+                    </span>
+                    <h3 class="highlight medium">Nuestra Visión</h3>
+                    <div class="border-paragraphs middle-borders bold bottommargin_0 greylinks">
+                        <p>
+                            <a href="#">ICI Industrial será una empresa innovadora
+                                en tecnología, flexible al cambio, líderes en automatización.
+                            </a>
+                        </p>
+                        <p>
+                            <a href="#">Empresa comprometida con el servicio al cliente.</a>
+                        </p>
+                        <p>
+                            <a href="#">Expandir nuestros servicios y productos a diferentes sectores
+                                industriales.</a>
+                        </p>
+                        <p>
+                            <a href="#">Empresa confiable para distribuir instrumentos en el sector
+                                industrial.</a>
+                        </p>
+                        <p>
+                            <a href="#">Crecer nuestra fabricación de sistemas de control con el más alto
+                                estándar de calidad en nuestros productos fabricados</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-offset-2 col-sm-8 col-md-offset-0 col-md-3">
+                <div class="with_padding text-center cs main_color2 with_bottom_border">
+                    <span class="image_icon">
+                        <img src="./images/icon3.png" alt="">
+                    </span>
+                    <h3 class="highlight medium">Objetivos</h3>
+                    <div class="border-paragraphs middle-borders bold bottommargin_0 greylinks">
+                        <p>
+                            <a href="#">Convertir nuestra empresa en una rentable para inversionistas.</a>
+                        </p>
+                        <p>
+                            <a href="#">Desarrollar a nuestro personal como primer paso al éxito.</a>
+                        </p>
+                        <p>
+                            <a href="#">Incrementar nuestra facturación al 20% anual./a>
+                        </p>
+                        <p>
+                            <a href="#">Desarrollar productos o proyectos sustentables y duraderos.</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="cs main_color2 section_padding_top_145 section_padding_bottom_130 columns_margin_bottom_30">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-offset-0 col-md-6 col-sm-offset-2 col-sm-8 text-center text-sm-left">
+                <h2 class="section_header">
+                    Novedades
+                </h2>
+                <hr class="divider_2_30 left main_bg_color topmargin_30">
+                <div class="inline-block bottommargin_50">
+                </div>
+                <br>
+                <a href="{{route('novedades.index')}}" class="theme_button color1">Mas Informacion</a>
+            </div>
+            <div class="col-lg-3 col-md-offset-0 col-md-6 col-sm-offset-2 col-sm-8">
+                <article class="vertical-item content-padding ls with_bottom_border topmargin_10">
+                    <div class="item-media">
+                        <img src="./images/accesoriosr-motortech.png" alt="">
+                        <div class="media-links">
+                            <div class="links-wrap">
+                                <a class="p-view prettyPhoto" title="" data-gal="prettyPhoto[gal]" href="./images/blog_post1.jpg"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-content">
+                        <div class="categories-links small-text big-spacing">
+                            <a href="#">Controladores</a>
+                        </div>
+                        <h3 class="entry-title">
+                            <a href="blog-single-right.html">MOTORTECH, principales fabricantes de controladores de encendido</a>
+                        </h3>
+                        <p>
+                            MOTORTECH se ha convertido en uno de los principales fabricantes de
+                            controladores de encendido en el mercado global para motores de gas industriales.
+                        </p>
+                    </div>
+                </article>
+            </div>
+            <div class="col-lg-3 col-md-offset-0 col-md-6 col-sm-offset-2 col-sm-8">
+                <article class="vertical-item content-padding ls with_bottom_border topmargin_10">
+                    <div class="item-media">
+                        <img src="./images/accesoriosr-motortech.png" alt="">
+                        <div class="media-links">
+                            <div class="links-wrap">
+                                <a class="p-view prettyPhoto" title="" data-gal="prettyPhoto[gal]" href="./images/blog_post2.jpg"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-content">
+                        <div class="categories-links small-text big-spacing">
+                            <a href="#">Controladores</a>
+                        </div>
+                        <h3 class="entry-title">
+                            <a href="blog-single-right.html">Tipos de Mantenimiento Industrial</a>
+                        </h3>
+                        <p>
+                            Es básico que todo profesional conozca los tipos de mantenimiento industrial existentes.
+                            Solo de esta forma podrá descubrir los pros y contras de cada uno y establecer un plan de mantenimiento adecuado para su empresa.
+                        </p>
+                    </div>
+                </article>
+            </div>
+            <div class="col-lg-3 col-md-offset-0 col-md-6 col-sm-offset-2 col-sm-8">
+                <article class="vertical-item content-padding ls with_bottom_border topmargin_10">
+                    <div class="item-media">
+                        <img src="./images/accesoriosr-motortech.png" alt="">
+                        <div class="media-links">
+                            <div class="links-wrap">
+                                <a class="p-view prettyPhoto" title="" data-gal="prettyPhoto[gal]" href="./images/blog_post3.jpg"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-content">
+                        <div class="categories-links small-text big-spacing">
+                            <a href="#">Controladores</a>
+                        </div>
+                        <h3 class="entry-title">
+                            <a href="blog-single-right.html">La automatización y sus aplicaciones en la industria</a>
+                        </h3>
+                        <p>
+                            El desarrollo e innovación de nuevas tecnologías,
+                            la automatización de procesos industriales, a través de los años, ha dado lugar a diversos avances significativos.
+                        </p>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="page_breadcrumbs ls ms ms_dark section_padding_25">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1 class="highlight2 bold bottommargin_5">Servicios</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="ls section_padding_top_135 section_padding_bottom_90 columns_padding_25 page_services">
+    <div class="container">
+        <div class="row columns_margin_bottom_40 services-row">
+            <div class="col-md-6">
+                <div class="teaser">
+                    <div class="main_bg_color2 size_normal round overlapped-icon">
+                        <!-- <i class="icon-thermal-power highlight"></i> -->
+                        <img src="./images/PANEL.png" alt="">
+                    </div>
+                    <h4 class="text-uppercase">TABLEROS DE CONTROL</h4>
+                    <hr class="divider_2_30 left main_bg_color">
+                    <p>
+                        Diseño de tableros de control para diferentes procesos,
+                        con capacidad de expansión a nuevas tecnologías en navegación web.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="teaser">
+                    <div class="main_bg_color2 size_normal round overlapped-icon">
+                        <!-- <i class="icon-oil-platform highlight"></i> -->
+                        <img src="./images/INFO.png" alt="">
+                    </div>
+                    <h4 class="text-uppercase">DESARROLLO DE SOFTWARE</h4>
+                    <hr class="divider_2_30 left main_bg_color">
+                    <p>
+                        Desarrollamos e implementamos
+                        ingeniería de software dedicados para optimizar sus procesos.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="teaser">
+                    <div class="main_bg_color2 size_normal round overlapped-icon">
+                        <!-- <i class="icon-gas-flaring highlight"></i> -->
+                        <img src="./images/ADMIN.png" alt="">
+                    </div>
+                    <h4 class="text-uppercase">INSTRUMENTACIÓN</h4>
+                    <hr class="divider_2_30 left main_bg_color">
+                    <p>
+                        Recomendación de instrumentación con última tecnología
+                        para garantizar su medición y control en procesos.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="teaser">
+                    <div class="main_bg_color2 size_normal round overlapped-icon">
+                        <!-- <i class="icon-oil-pump highlight"></i> -->
+                        <img src="./images/MANT3.png" alt="">
+                    </div>
+                    <h4 class="text-uppercase">INSTALACIÓN</h4>
+                    <hr class="divider_2_30 left main_bg_color">
+                    <p>
+                        Instalación flexible de los instrumentos,
+                        para reducir horas de mantenimiento preventivo y correctivo..
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="teaser">
+                    <div class="main_bg_color2 size_normal round overlapped-icon">
+                        <!-- <i class="icon-oil-refinery highlight"></i> -->
+                        <img src="./images/BOMBEAR.png" alt="">
+                    </div>
+                    <h4 class="text-uppercase">DESARROLLO DE APLICACIONES</h4>
+                    <hr class="divider_2_30 left main_bg_color">
+                    <p>
+                        Desarrollo de aplicaciones, como Bomba, Compresores, Generadores,
+                        líneas de producción, con datos de almacenamiento para mejorar su rendimiento.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="teaser">
+                    <div class="main_bg_color2 size_normal round overlapped-icon">
+                        <!-- <i class="icon-factory highlight"></i> -->
+                        <img src="./images/MANT.png" alt="">
+                    </div>
+                    <h4 class="text-uppercase">CALIBRACION Y MANTENIMIENTO</h4>
+                    <hr class="divider_2_30 left main_bg_color">
+                    <p>
+                        Servicios de calibración
+                        y mantenimiento con herramienta
+                        certificada con trazabilidad al CENAM.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<iframe id="map" style="border:0; width: 100%;" class="ls page_map" data-address="40 Blandford St Marylebone, London" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d949.2798663248235!2d-92.9242673!3d17.8798666!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85edda890e4e81a3%3A0xb95f3b16ade2d69!2sICI%20INDUSTRIAL!5e0!3m2!1sen!2smx!4v1619731819276!5m2!1sen!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
+</iframe>
+
+
+
+@endsection
